0398d085bf9a6c896d5d8e916550db4f71a7f5f2
Gustavo Tejerina Santos <39309543+gustavoprimitive@users.noreply.github.com>
2018-10-28 22:58:10 +0100

    Add files via upload

--- PL_generar_csv_desde_tabla.sql
+++ PL_generar_csv_desde_tabla.sql
@@ -3,18 +3,21 @@
 
 DECLARE
 
-  v_fichero     UTL_FILE.FILE_TYPE;
-  v_file_name   VARCHAR2(50) := 'salida.csv'; --Nombre de fichero de salida
-  v_dir         VARCHAR2(50) := 'DIR'; --Nombre de directorio
-  v_table       VARCHAR2(50) := 'EMPLOYEES'; --Nombre de tabla a exportar
-  v_where       VARCHAR2(500) := 'WHERE ROWNUM <= 10'; --Cláusula where a ejecutar (si es el caso)
-  v_sep         CHAR(1) := ';'; --Separador en CSV
-  v_query       VARCHAR2(500);
+  v_file_name CONSTANT VARCHAR2(50) := 'salida.csv'; --Nombre de fichero de salida
+  v_dir       CONSTANT VARCHAR2(50) := 'DIR'; --Nombre de directorio
+  v_table     CONSTANT VARCHAR2(50) := 'EMPLOYEES'; --Nombre de tabla a exportar
+  v_where     CONSTANT VARCHAR2(500) := 'WHERE ROWNUM <= 10'; --Cláusula where a ejecutar (si es el caso)
+  v_sep       CONSTANT CHAR(1) := ';'; --Separador en CSV
+  v_query     VARCHAR2(500);
+  v_csv_file   utl_file.file_type;
   TYPE t_out IS REF CURSOR;
-  cur_out         t_out;
-  v_line        VARCHAR2(20000);
-  v_count_cols  NUMBER;
-  v_path        VARCHAR2(200);
+  cur_out      t_out;
+  v_line       VARCHAR2(20000);
+  v_count_cols NUMBER;
+  v_path       VARCHAR2(200);
+  v_txt_error  VARCHAR2(500);
+  e_error EXCEPTION;
+  
   CURSOR c_query(v_tab VARCHAR2) IS
     SELECT column_name
       FROM all_tab_cols
@@ -28,14 +31,25 @@ BEGIN
     INTO v_count_cols
     FROM all_tab_cols
    WHERE table_name = UPPER(v_table);
+
+  IF v_count_cols = 0 THEN
+    v_txt_error := 'ERROR. No se encuentra la tabla ' || v_table;
+    RAISE e_error;
+  END IF;
+
   --Obtención de path de directorio Oracle
-  SELECT directory_path
-    INTO v_path
-    FROM all_directories
-   WHERE directory_name = UPPER(v_dir);
+  BEGIN
+    SELECT directory_path
+      INTO v_path
+      FROM all_directories
+     WHERE directory_name = UPPER(v_dir);
+  EXCEPTION
+    WHEN OTHERS THEN
+      v_txt_error := 'ERROR. No se encuentra el directorio ' || v_dir;
+      RAISE e_error;
+  END;
 
   --Construcción de consulta para cursor variable
-  v_table := UPPER(v_table);
   v_query := 'SELECT ';
   FOR r_query IN c_query(v_table) LOOP
     IF c_query%ROWCOUNT ^= v_count_cols THEN
@@ -47,29 +61,53 @@ BEGIN
 
   --Para añadir cláusula where si es el caso
   IF v_where IS NULL THEN
-    v_query := v_query || ' FROM ' || UPPER(v_table);
+    v_query := v_query || ' FROM ' || v_table;
   ELSE
-    v_query := v_query || ' FROM ' || UPPER(v_table) || ' ' || v_where;
+    v_query := v_query || ' FROM ' || v_table || ' ' || v_where;
   END IF;
 
-  DBMS_OUTPUT.PUT_LINE('- Fichero a generar: ' || v_path || v_file_name);
-  DBMS_OUTPUT.PUT_LINE('- Query a emplear: ' || v_query);
+  dbms_output.put_line('- Fichero a generar: ' || v_path || v_file_name);
+  dbms_output.put_line('- Query a emplear: ' || v_query);
 
   --Apertura de fichero
-  v_fichero := UTL_FILE.FOPEN(v_dir, v_file_name, 'W');
+  BEGIN
+    v_csv_file := utl_file.fopen(v_dir, v_file_name, 'W');
+  EXCEPTION
+    WHEN OTHERS THEN
+      v_txt_error := 'ERROR al generar el fichero ' || v_file_name || ' en ' || v_dir;
+      RAISE e_error;
+  END;
 
   --Ejecución de query y escritura de resultados de la misma en CSV
-  DBMS_OUTPUT.PUT_LINE('- Datos en fichero:');
-  OPEN cur_out FOR v_query;
-  LOOP
-    FETCH cur_out INTO v_line;
-    EXIT WHEN cur_out%NOTFOUND;
-    DBMS_OUTPUT.PUT_LINE(v_line);
-    UTL_FILE.PUT_LINE(v_fichero, v_line);
-  END LOOP;
+  dbms_output.put_line('- Datos en fichero:');
+
+  BEGIN
+    OPEN cur_out FOR v_query;
+  EXCEPTION
+    WHEN OTHERS THEN
+      v_txt_error := 'ERROR al ejecutar la consulta';
+      RAISE e_error;
+  END;
+
+  BEGIN
+    LOOP
+      FETCH cur_out INTO v_line;
+      EXIT WHEN cur_out%NOTFOUND;
+      dbms_output.put_line(v_line);
+      utl_file.put_line(v_csv_file, v_line);
+    END LOOP;
+  EXCEPTION
+    WHEN OTHERS THEN
+      v_txt_error := 'ERROR al escribir el fichero ' || v_file_name || ' en ' || v_path;
+  END;
 
   --Cierre de fichero
-  UTL_FILE.FCLOSE(v_fichero);
+  utl_file.fclose(v_csv_file);
 
+EXCEPTION
+  WHEN e_error THEN
+    dbms_output.put_line(chr(9) || v_txt_error);
+  
 END;
 /
+

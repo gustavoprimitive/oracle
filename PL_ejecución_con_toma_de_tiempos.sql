d29085430dab013767b6ca6fe7cd5e65da97b1a7
Gustavo Tejerina Santos <39309543+gustavoprimitive@users.noreply.github.com>
2018-11-06 14:02:00 +0100

    Update PL_ejecución_con_toma_de_tiempos.sql

--- PL_ejecución_con_toma_de_tiempos.sql
+++ PL_ejecución_con_toma_de_tiempos.sql
@@ -1,9 +1,9 @@
---Itera una ejecución el número de veces deseado para obtener trazas con los tiempos invertidos
+--Itera una ejecuciÃ³n el nÃºmero de veces deseado para obtener trazas con los tiempos invertidos
 --Gustavo Tejerina
 
 DECLARE
 
-  v_num_iter NUMBER := '&v_num_iter'; --Nº de ejecuciones a realizar
+  v_num_iter NUMBER := '&v_num_iter'; --NÂº de ejecuciones a realizar
   v_ini      TIMESTAMP;
   v_fin      TIMESTAMP;
   v_interv   INTERVAL DAY(1) TO SECOND(6);
@@ -15,28 +15,28 @@ BEGIN
 
   FOR i IN 1 .. v_num_iter LOOP
   
-    --Momento de inicio de ejecución
+    --Momento de inicio de ejecuciÃ³n
     v_ini := systimestamp;
   
     --Traza
-    dbms_output.put_line(v_ini || chr(9) || 'INFO' || chr(9) || 'Ejecución #' || i);
+    dbms_output.put_line(v_ini || chr(9) || 'INFO' || chr(9) || 'EjecuciÃ³n #' || i);
   
-    --Aquí iría el código del que se quieren registrar los tiempos ->
-    --Generación de delay de entre 1 y 3 segundos
+    --AquÃ­ irÃ­a el cÃ³digo del que se quieren registrar los tiempos ->
+    --GeneraciÃ³n de delay de entre 1 y 3 segundos
     dbms_lock.sleep(dbms_random.value(1, 3));
-    -- <- Aquí iría el código del que se quieren registrar los tiempos
+    -- <- AquÃ­ irÃ­a el cÃ³digo del que se quieren registrar los tiempos
   
-    --Momento de fin de ejecución
+    --Momento de fin de ejecuciÃ³n
     v_fin := systimestamp;
   
-    --Obtención de intervalo
+    --ObtenciÃ³n de intervalo
     v_interv   := v_fin - v_ini;
     v_horas    := extract(hour FROM v_interv);
     v_minutos  := extract(minute FROM v_interv);
-    v_segundos := extract(SECOND FROM v_interv);
+    v_segundos := extract(second FROM v_interv);
   
     --Traza
-    dbms_output.put_line(chr(9) || v_horas || ' hora(s) ' || v_minutos || ' minuto(s) ' || v_segundos || ' segundo(s)' || dchr(10));
+    dbms_output.put_line(chr(9) || v_horas || ' hora(s) ' || v_minutos || ' minuto(s) ' || v_segundos || ' segundo(s)' || chr(10));
   
   END LOOP;
 

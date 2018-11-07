47b8b3c740527784789bb648dc768de434b39009
Gustavo Tejerina Santos <39309543+gustavoprimitive@users.noreply.github.com>
2018-11-07 09:55:19 +0100

    Update PL_matar_sesión_a_partir_de_ID_de_proceso.sql

--- PL_matar_sesión_a_partir_de_ID_de_proceso.sql
+++ PL_matar_sesión_a_partir_de_ID_de_proceso.sql
@@ -1,5 +1,7 @@
---Finaliza una sesión Oracle a partir del nombre del servidor y del ID del proceso de dicha sesión.
+--Finaliza una sesiÃ³n Oracle a partir del nombre del servidor y del ID del proceso de dicha sesiÃ³n.
 --Gustavo Tejerina
+
+SET SERVEROUTPUT ON
 
 DECLARE
 
@@ -11,15 +13,15 @@ BEGIN
   SELECT s.sid, s.serial#
     INTO v_sid, v_serial
     FROM v$session s, v$process p, v$instance i
-   WHERE p.spid = '&v_proceso' --Nº de proceso en S.O.
+   WHERE p.spid = '&v_proceso' --NÂº de proceso en S.O.
      AND s.paddr = p.addr
-     AND UPPER(i.host_name) = UPPER('&v_host'); --Hostname de máquina
+     AND UPPER(i.host_name) = UPPER('&v_host'); --Hostname de mÃ¡quina
 
   EXECUTE IMMEDIATE 'ALTER SYSTEM KILL SESSION ''' || v_sid || ',' || v_serial || ''' IMMEDIATE';
 
 EXCEPTION
   WHEN NO_DATA_FOUND THEN
-    dbms_output.put_line('Proceso no encontrado');
+    dbms_output.put_line('ERROR. Proceso o host no encontrados');
   WHEN OTHERS THEN
     dbms_output.put_line(SQLERRM);
 END;

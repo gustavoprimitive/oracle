a6a3c8257d1cac7ce055cadc38dadaeb0cb2011d
Gustavo Tejerina Santos <39309543+gustavoprimitive@users.noreply.github.com>
2018-11-07 10:29:02 +0100

    Update PL_matar_sesión_a_partir_de_ID_de_proceso.sql

--- PL_matar_sesión_a_partir_de_ID_de_proceso.sql
+++ PL_matar_sesión_a_partir_de_ID_de_proceso.sql
@@ -5,6 +5,7 @@ SET SERVEROUTPUT ON
 
 DECLARE
 
+  v_proceso NUMBER := '&v_proceso'; --Nº de proceso en S.O. 
   v_sid    v$session.sid%TYPE;
   v_serial v$process.serial#%TYPE;
 
@@ -12,16 +13,15 @@ BEGIN
 
   SELECT s.sid, s.serial#
     INTO v_sid, v_serial
-    FROM v$session s, v$process p, v$instance i
-   WHERE p.spid = '&v_proceso' --Nº de proceso en S.O.
-     AND s.paddr = p.addr
-     AND UPPER(i.host_name) = UPPER('&v_host'); --Hostname de máquina
+    FROM v$session s, v$process p
+   WHERE regexp_substr(s.process,'^[0-9]+') = v_proceso   
+     AND s.paddr = p.addr;
 
   EXECUTE IMMEDIATE 'ALTER SYSTEM KILL SESSION ''' || v_sid || ',' || v_serial || ''' IMMEDIATE';
 
 EXCEPTION
   WHEN NO_DATA_FOUND THEN
-    dbms_output.put_line('ERROR. Proceso o host no encontrados');
+    dbms_output.put_line('ERROR. Proceso no encontrado');
   WHEN OTHERS THEN
     dbms_output.put_line(SQLERRM);
 END;

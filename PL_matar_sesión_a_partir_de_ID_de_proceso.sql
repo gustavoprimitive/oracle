b0afa52ab4c89f14f29c277c9e22ee10c8cfb7be
Gustavo Tejerina Santos <39309543+gustavoprimitive@users.noreply.github.com>
2018-11-07 11:57:55 +0100

    Update PL_matar_sesión_a_partir_de_ID_de_proceso.sql

--- PL_matar_sesión_a_partir_de_ID_de_proceso.sql
+++ PL_matar_sesión_a_partir_de_ID_de_proceso.sql
@@ -1,4 +1,4 @@
---Finaliza una sesión Oracle a partir del nombre del servidor y del ID del proceso de dicha sesión.
+--Finaliza una sesión Oracle a partir del ID en el S.O. del proceso de dicha sesión.
 --Gustavo Tejerina
 
 SET SERVEROUTPUT ON
@@ -6,22 +6,53 @@ SET SERVEROUTPUT ON
 DECLARE
 
   v_proceso NUMBER := &v_proceso; --Nº de proceso en S.O. 
-  v_sid    v$session.sid%TYPE;
-  v_serial v$process.serial#%TYPE;
+  v_sid     v$session.sid%TYPE;
+  v_serial  v$process.serial#%TYPE;
+  v_status  v$session.state%TYPE;
+  v_check   NUMBER DEFAULT 0;
+
+  --Proceso local para extraer los IDs del proceso en Oracle y su estado 
+  PROCEDURE p_datos_proceso(v_proceso NUMBER,
+                            v_sid     OUT v$session.sid%TYPE,
+                            v_serial  OUT v$session.serial#%TYPE,
+                            v_status  OUT v$session.state%TYPE) AS
+  BEGIN
+  
+    SELECT s.sid, s.serial#, s.state
+      INTO v_sid, v_serial, v_status
+      FROM v$session s, v$process p
+     WHERE regexp_substr(s.process, '^[0-9]+') = v_proceso
+       AND s.paddr = p.addr;
+  
+  EXCEPTION
+    WHEN OTHERS THEN
+      v_sid    := NULL;
+      v_serial := NULL;
+      v_status := NULL;
+  END;
 
 BEGIN
 
-  SELECT s.sid, s.serial#
-    INTO v_sid, v_serial
-    FROM v$session s, v$process p
-   WHERE regexp_substr(s.process,'^[0-9]+') = v_proceso   
-     AND s.paddr = p.addr;
+  p_datos_proceso(v_proceso, v_sid, v_serial, v_status);
 
-  EXECUTE IMMEDIATE 'ALTER SYSTEM KILL SESSION ''' || v_sid || ',' || v_serial || ''' IMMEDIATE';
+  --Si se han obtenido los datos del proceso, se finaliza la sesión
+  IF v_sid IS NOT NULL AND v_serial IS NOT NULL THEN
+    EXECUTE IMMEDIATE 'ALTER SYSTEM KILL SESSION ''' || v_sid || ',' || v_serial || ''' IMMEDIATE';
+  
+    --Se comprueba si se ha finalizado la sesión
+    p_datos_proceso(v_proceso, v_sid, v_serial, v_status);
+  
+    IF v_status IS NULL THEN
+      dbms_output.put_line('Proceso finalizado');
+    ELSE
+      dbms_output.put_line('El proceso se mantiene en estado ' || v_status);
+    END IF;
+  
+  ELSE
+    dbms_output.put_line('ERROR. Proceso no encontrado');
+  END IF;
 
 EXCEPTION
-  WHEN NO_DATA_FOUND THEN
-    dbms_output.put_line('ERROR. Proceso no encontrado');
   WHEN OTHERS THEN
     dbms_output.put_line(SQLERRM);
 END;
